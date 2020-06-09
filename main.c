b23aedbc68fc00055664426b4456e89d961a6eb7
DomAsw15 <asw15@orange.fr>
2020-06-09 19:23:40 +0200

    Auto formation test github 03

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 /* tri un tableau de 4 entiers en ordre croissant */
 /* le 7 juin 2020 DomAsw15 */
-/* Auto formation test github 02*/
+/* Auto formation test github 03*/
 
 int main(int argc, char *argv[])
 {
